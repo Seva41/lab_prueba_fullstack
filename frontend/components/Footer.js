ac948ae49e3082efd3204cfc4141f24cf30a259d
Sebastian Dinator <sdinator@alumnos.uai.cl>
2025-02-26 16:13:59 -0300

    Footer

---
+++ frontend/components/Footer.js
@@ -0,0 +1,16 @@
+export default function Footer() {
+    return (
+      <footer className="app-footer">
+        <p>
+          Prueba Fullstack - Pokémon TCG API y Frontend - Hecho por{" "}
+          <a
+            href="https://github.com/Seva41/lab_prueba_fullstack"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Sebastián Dinator
+          </a>
+        </p>
+      </footer>
+    );
+  }
